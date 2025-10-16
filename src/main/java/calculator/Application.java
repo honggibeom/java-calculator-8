fb299b4988d0132241c6395d7fa9d58a20fc9d5e
honggibeom <90910169+honggibeom@users.noreply.github.com>
2025-10-16 16:24:20 +0900

    feat: 입출력 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String str = Console.readLine();
+        DelimiterStrategy delimiterStrategy  = new DelimiterStrategy();
+        delimiterStrategy.setStrategy(DelimiterStrategyFactory.create(str));
+
+        String numbers =  delimiterStrategy.extractNumbers(str);
+
+        if(!Validator.isValid(numbers, str)) {
+            throw new IllegalArgumentException();
+        }
+
+        int result = Calculator.sum(numbers);
+        System.out.println("결과 : "+result);
     }
 }
