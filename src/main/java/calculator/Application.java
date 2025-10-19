ee81f836a6274658d8cc705248071c497d3408ce
honggibeom <90910169+honggibeom@users.noreply.github.com>
2025-10-19 18:32:32 +0900

    feat: 출력 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String str = Console.readLine();
+        DelimiterStrategy delimiterStrategy  = new DelimiterStrategy();
+        delimiterStrategy.setStrategy(DelimiterStrategyFactory.create(str));
+
+        String numbers =  delimiterStrategy.extractNumbers(str);
+
+        Validator.doValidation(numbers, str);
+
+        int result = Calculator.sum(numbers);
+        System.out.println("결과 : "+result);
     }
 }
