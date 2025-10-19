3110e0b5956b272d5ff52a59a831b820e0cda489
honggibeom <90910169+honggibeom@users.noreply.github.com>
2025-10-19 22:29:17 +0900

    test: 기능 테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -2,7 +2,6 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -24,8 +23,88 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 전제조건1() {
+        assertSimpleTest(() -> {
+            run("//;w;\\n1;w;2");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+
+    @Test
+    void 전제조건2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//,2\\n1,222"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 전제조건3() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;;\\n\\n2;;\\n\\n2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 문자열처리_case1() {
+        assertSimpleTest(() -> {
+            run("//,sw\\n1,sw2,5,sw6");
+            assertThat(output()).contains("결과 : 14");
+        });
+    }
+
+    @Test
+    void 문자열처리_case2() {
+        assertSimpleTest(() -> {
+            run("1,2,32");
+            assertThat(output()).contains("결과 : 35");
+        });
+    }
+
+    @Test
+    void 입력값검증_case1_1() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,-2,3,-4"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 입력값검증_case1_2() {
+        assertSimpleTest(() -> {
+            run("1,2,3,4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void 입력값검증_case2_1_1() {
+        assertSimpleTest(() -> {
+            run("//;;;\\n2;;;3,1");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 입력값검증_case2_1_2() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("//\\n2,3,1"))
+                    .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 입력값검증_case2_2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("/;;;\\n2;;;3,1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Override
     public void runMain() {
-        Application.main(new String[]{});
+            Application.main(new String[]{});
     }
 }
